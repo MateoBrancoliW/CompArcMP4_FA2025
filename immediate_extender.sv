7cfddb272d069b3a79d79cd0235c27e2f870fa84
MateoBrancoliW <145402691+MateoBrancoliW@users.noreply.github.com>
2025-12-07 15:01:02 -0500

    added U type and 3 bit encodings

--- immediate_extender.sv
+++ immediate_extender.sv
@@ -7,10 +7,11 @@ module immediate_extension(
 
     always_comb begin
         case (imm_src)
-            2'b00: imm_ext = {{20{instr[31]}}, instr[31:20]};  // I-type
-            2'b01: imm_ext = {{20{instr[31]}}, instr[31:25], instr[11:7]};  // S-type
-            2'b10: imm_ext = {{20{instr[31]}},instr[7],instr[30:25], instr[11:8], 1'd0};  // B-type
-            2'b11: imm_ext = {{12{instr[31]}}, instr[19:12], instr[20], instr[30:21], 1'd0};  // J-type
+            3'b000: imm_ext = {{20{instr[31]}}, instr[31:20]};  // I-type
+            3'b001: imm_ext = {{20{instr[31]}}, instr[31:25], instr[11:7]};  // S-type
+            3'b010: imm_ext = {{20{instr[31]}},instr[7],instr[30:25], instr[11:8], 1'd0};  // B-type
+            3'b011: imm_ext = {{12{instr[31]}}, instr[19:12], instr[20], instr[30:21], 1'd0};  // J-type
+            3'b100: imm_ext = {{19{instr[31]}}, instr[31], instr[7], instr[30:25], instr[11:8], 1'b0};  // U-type
             default: imm_ext = 32'd0;
         endcase
     end

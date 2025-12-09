0e10179ebd886e78f3d73e236da4e4ed34a01d37
systamii <jayceetrees411@gmail.com>
2025-12-08 22:48:58 -0500

    alu edits

--- alu.sv
+++ alu.sv
@@ -1,17 +1,18 @@
 `timescale 1ns/1ps
 
 // ALU operation encodings
-`define ALU_ADD 3'b000
-`define ALU_SUB 3'b001
-`define ALU_AND 3'b010
-`define ALU_OR  3'b011
-`define ALU_XOR 3'b100
-`define ALU_SLL 3'b101
-`define ALU_SRL 3'b110
-`define ALU_SRA 3'b111
-
+`define ALU_ADD 4'b0000
+`define ALU_SUB 4'b0001
+`define ALU_AND 4'b0010
+`define ALU_OR  4'b0011
+`define ALU_XOR 4'b0100
+`define ALU_SLL 4'b1000
+`define ALU_SRL 4'b0110
+`define ALU_SRA 4'b0111
+`define ALU_SLT 4'b0101
+`define ALU_SLTU 4'b1001
 module alu(
-    input  logic [2:0]  alu_operation,
+    input  logic [3:0]  alu_operation,
     input  logic [31:0] alu_a,
     input  logic [31:0] alu_b,
     output logic [31:0] alu_c,
@@ -22,7 +23,7 @@ module alu(
 
     always_comb begin
         alu_c = 32'b0;
-
+        zero = 0;
         case (alu_operation)
             `ALU_ADD: alu_c = alu_a + alu_b;
             `ALU_SUB: alu_c = alu_a - alu_b;
@@ -32,12 +33,16 @@ module alu(
             `ALU_SLL: alu_c = alu_a <<  alu_b[4:0];
             `ALU_SRL: alu_c = alu_a >>  alu_b[4:0];
             `ALU_SRA: alu_c = alu_a >>> alu_b[4:0];
-            default:  alu_c = 32'b0;
+            `ALU_SLT: begin
+                signed_less_than = $signed(SrcA) < $signed(SrcB);
+                zero = alu_c[0];
+            end
+            `ALU_SLTU: begin
+                alu_c = $unsigned(SrcA) < $unsigned(SrcB);
+                Zero = alu_c[0];
+            end
+            default: begin
+            end
         endcase
     end
-
-    assign zero = (alu_c == 32'b0);
-    assign signed_less_than = ($signed(alu_a) < $signed(alu_b));
-    assign less_than = (alu_a < alu_b);
-
 endmodule: alu
